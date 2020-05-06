72930ad274fe7a3bacc84ee561ce22853f141c08
Matt Eland <matt.eland@gmail.com>
2020-05-06 15:01:26 -0400

    Grid generation

--- src/com/techelevator/matteland/GridGenerator.java
+++ src/com/techelevator/matteland/GridGenerator.java
@@ -1,13 +1,23 @@
 package com.techelevator.matteland;
 
+import java.util.Random;
+
 public class GridGenerator {
     public WordGrid generate(int sizeX, int sizeY) {
         WordGrid grid = new WordGrid(sizeX, sizeY);
 
-        // TODO: Randomly set letters in the grid
+        final int lengthOfAlphabet = 26;
+        final int uppercaseLetterOffset = 65; // Where in the ASCII codes the uppercase letters start
+
+        final Random random = new Random();
+
+        // Randomly set letters in the grid
         for (int y = 0; y < sizeY; y++) {
             for (int x = 0; x < sizeX; x++) {
-                grid.setCharacter(x, y, 'A');
+                // Generate a good ASCII code and then convert it to a character
+                char letter = (char) (random.nextInt(lengthOfAlphabet) + uppercaseLetterOffset);
+
+                grid.setCharacter(x, y, letter);
             }
         }
 
